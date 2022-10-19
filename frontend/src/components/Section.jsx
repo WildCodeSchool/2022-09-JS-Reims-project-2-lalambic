79a73902d8f05986e6b4e225862bc2374b87e059
farid <faridhimeur@outlook.fr>
2022-10-19 17:35:25 +0200

    change Section.jsx

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -5,8 +5,20 @@ function Section() {
       <div className="display-main">
         <div className="card">
           
-          <img href="https://media.istockphoto.com/photos/cocktail-splash-picture-id1283952381" alt="it's a cocktail" />
-          <img href="https://media.istockphoto.com/photos/preparation-of-a-cocktail-picture-id1340608319" alt="it's a cocktail" />
+          <img 
+
+          href="https://media.istockphoto.com/photos/cocktail-splash-picture-id1283952381"
+          alt="it's a cocktail" 
+          
+          />
+
+          
+          <img
+
+          href="https://media.istockphoto.com/photos/preparation-of-a-cocktail-picture-id1340608319"
+          alt="it's a cocktail" 
+          
+          />
 
           </div>
       </div>
