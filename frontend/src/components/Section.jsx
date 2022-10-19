7b1fb2fb624bc5f1fb716b207d09128d96ec3a85
duarte51 <duarte.02.alves.jose@gmail.com>
2022-10-19 16:15:08 +0200

    Add img in Section2 Header.jsx

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -2,7 +2,15 @@ function Section() {
   return (
     <div className="section">
       <section className="display-selection">section1</section>
-      <section className="display-main">section2</section>
+      <section className="display-main">
+        <div>
+          
+          <img href="https://media.istockphoto.com/photos/cocktail-splash-picture-id1283952381" alt="it's a cocktail" />
+          <img href="https://media.istockphoto.com/photos/preparation-of-a-cocktail-picture-id1340608319" alt="it's a cocktail" />
+
+          </div>
+      </section>
+
     </div>
   );
 }
