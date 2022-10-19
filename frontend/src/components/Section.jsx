692def0be92338fd683232e645a86468eb5bd64c
farid <faridhimeur@outlook.fr>
2022-10-19 17:19:53 +0200

    change section to div

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -1,15 +1,15 @@
 function Section() {
   return (
     <div className="section">
-      <section className="display-selection">section1</section>
-      <section className="display-main">
+      <div className="display-selection">section1</div>
+      <div className="display-main">
         <div>
           
           <img href="https://media.istockphoto.com/photos/cocktail-splash-picture-id1283952381" alt="it's a cocktail" />
           <img href="https://media.istockphoto.com/photos/preparation-of-a-cocktail-picture-id1340608319" alt="it's a cocktail" />
 
           </div>
-      </section>
+      </div>
 
     </div>
   );
