ca9b65f39cb2488e278346dc67788e16b665a7ad
farid <faridhimeur@outlook.fr>
2022-10-19 17:22:51 +0200

    add title section2

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -3,7 +3,7 @@ function Section() {
     <div className="section">
       <div className="display-selection">section1</div>
       <div className="display-main">
-        <div>
+        <div className="card">
           
           <img href="https://media.istockphoto.com/photos/cocktail-splash-picture-id1283952381" alt="it's a cocktail" />
           <img href="https://media.istockphoto.com/photos/preparation-of-a-cocktail-picture-id1340608319" alt="it's a cocktail" />
