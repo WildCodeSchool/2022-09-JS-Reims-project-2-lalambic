6ce5110a6489076eec2173b03ec8dddf3782a2df
Oliv751 <olivierlopez751@gmail.com>
2022-10-19 17:08:00 +0200

    create an array for data cocktails

---
+++ frontend/src/components/DataCocktail.jsx
@@ -0,0 +1,62 @@
+const dataCocktail = [
+  {
+    name: "Margarita",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+    description:
+      "A classic margarita is made with tequila, triple sec, and lime juice. It is served shaken with ice (on the rocks), blended with ice (frozen margarita), or without ice (straight up).",
+    instructions:
+      "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..",
+    ingredients: ["Tequila", "Triple Sec", "Lime Juice", "salt"],
+    measurements: ["1 1/2 oz ", "1/2 oz ", "1 oz "],
+  },
+  {
+    name: "Blue Margarita",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/bry4qh1582751040.jpg",
+    description:
+      "A blue margarita is a margarita cocktail that has been colored blue with blue curacao.",
+    instructions:
+      "Rub rim of cocktail glass with lime juice. Dip rim in coarse salt. Shake all ingredients with ice, strain into the salt-rimmed glass, and serve.",
+    ingredients: ["Tequila", "Blue Curacao", "Lime Juice", "salt"],
+    measurements: ["1 1/2 oz ", "1/2 oz ", "1 oz "],
+  },
+  {
+    name: "Tommy's Margarita",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg",
+    description: "A very strong margarita.",
+    instructions: "Shake and strain into a chilled cocktail glass.",
+    ingredients: ["Tequila", "Triple Sec", "Agave syrup"],
+    measurements: ["1 1/2 oz ", "1/2 oz ", "1 tsp "],
+  },
+  {
+    name: "Whitecap Margarita",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg",
+    description: "A very strong margarita.",
+    instructions:
+      "Place all ingredients in a blender and blend until smooth. This makes one drink.",
+    ingredients: ["Ice", "Tequila", "Cream of coconut", "Lime Juice"],
+    measurements: ["1 cup ", "2 oz ", "1/4 cup ", "3 tblsp fresh  "],
+  },
+  {
+    name: "Strawberry Margarita",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg",
+    description: "A very strong margarita.",
+    instructions:
+      "Rub rim of cocktail glass with lemon juice and dip rim in salt. Shake schnapps, tequila, triple sec, lemon juice, and strawberries with ice, strain into the salt-rimmed glass, and serve.",
+    ingredients: [
+      "Strawberry schnapps",
+      "Tequila",
+      "Triple Sec",
+      "lemon juice",
+      "Strawberry schnapps",
+      "salt",
+    ],
+    measurements: ["1/2 oz ", "1 oz ", "1/2 oz ", "1 oz ", "1 oz "],
+  },
+];
+
+export default dataCocktail;
