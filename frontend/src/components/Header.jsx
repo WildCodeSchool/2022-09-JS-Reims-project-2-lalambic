0f3b7d6547403434a07d1afb39f1e5dbdfcfc21f
jessy hazart <jessy.hazart@gmail.com>
2022-10-21 10:04:02 +0200

    Louen's commit of header css style and imported assets

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,15 +3,19 @@ import SearchBar from "./SearchBar";
 function Header() {
   return (
     <div className="header">
-      <button type="button">Lang</button>
-      <img src="#" alt="logo" />
+      <button type="button" className="lang">
+        Lang
+      </button>
+      <img
+        src="../src/assets/logo-texte.png"
+        alt="logo"
+        className="logo-header"
+      />
       <SearchBar />
       <nav>
         <ul className="filter">
           <li>Nom</li>
-          <li>
-            Ingrédients <SearchBar />{" "}
-          </li>
+          <li>Ingrédients</li>
           <li>Type de verre</li>
           <li>Catégories</li>
           <li>Avec ou sans alcool</li>
