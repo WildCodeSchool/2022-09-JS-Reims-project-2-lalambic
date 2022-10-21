f780148c10ec5f28be3db634b642c16a1628ba09
jessy hazart <jessy.hazart@gmail.com>
2022-10-21 09:27:57 +0200

    added two cards and names of cocktails (Lucie commit)

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -1,18 +1,36 @@
 function Section() {
   return (
     <div className="section">
-      <div className="display-selection">section1</div>
+       <div className="display-selection">
+        <div className="card">
+          <img
+            src="https://www.thecocktaildb.com/images/media/drink/59splc1504882899.jpg"
+            alt="Havana Cocktail"
+          />
+          <h2>Havana Cocktail</h2>
+        </div>
+        <div className="card">
+          <img
+            src="https://www.thecocktaildb.com/images/media/drink/0bkwca1492975553.jpg"
+            alt="Highland Fling Cocktail"
+          />
+          <h2>Highland Fling Cocktail</h2>
+        </div>
+      </div>
       <div className="display-main">
         <div className="card">
           <img
             src="https://media.istockphoto.com/photos/cocktail-splash-picture-id1283952381"
             alt="it's a cocktail"
           />
-
+          <h2>Highland Fling Cocktail</h2>
+        </div>
+        <div className="card">
           <img
             src="https://media.istockphoto.com/photos/preparation-of-a-cocktail-picture-id1340608319"
             alt="it's a cocktail"
           />
+          <h2>Highland Fling Cocktail</h2>
         </div>
       </div>
     </div>
