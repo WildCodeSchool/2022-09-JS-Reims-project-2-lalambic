19662da63b7492871e31ae5322ba9d9b6d7b52c3
jessy hazart <jessy.hazart@gmail.com>
2022-11-15 11:56:02 +0100

    cleaned the function fetchApi in allCocktails.js

--- frontend/src/data/allCocktails.js
+++ frontend/src/data/allCocktails.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const downloadedCocktails = [];
-
 const alphabet = "abcdefghijklmnopqrstvwyz".split("");
 
 const useFetch = () => {
@@ -10,51 +8,55 @@ const useFetch = () => {
   const [cocktails, setCocktails] = useState([]);
 
   function fetchApi() {
-    const promises = [];
     /* fetch the api for each alphabet letter(excepted for u and x) to download all cocktails data */
-    alphabet.forEach((letter) => {
-      const fetch = axios
-        .get(
+    Promise.all(
+      alphabet.map((letter) =>
+        axios.get(
           `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
         )
-        .then((res) => res);
-      promises.push(fetch);
-      if (promises.length === 24) {
-        /* if all promises are inserted into an array, set new keys for values of all cocktails */
-        Promise.all([...promises]).then((res) => {
-          res.forEach((data) =>
-            data.data.drinks.forEach((cocktail) => {
-              const newCocktail = {
-                id: cocktail.idDrink,
-                name: cocktail.strDrink,
-                category: cocktail.strCategory,
-                alcoholic: cocktail.strAlcoholic,
-                glass: cocktail.strGlass,
-                instructions: cocktail.strInstructions,
-                image: cocktail.strDrinkThumb,
-                ingredients: [],
-                measures: [],
-              };
-              /* insert ingredients and measures in their arrays */
-              for (const [key, value] of Object.entries(cocktail)) {
-                if (key.includes("strIngredient") && value != null) {
-                  newCocktail.ingredients.push(value);
-                }
-                if (key.includes("strMeasure") && value != null) {
-                  newCocktail.measures.push(value);
-                }
-              }
-              /* insert cocktail object in an array if all cocktails are not downloaded and stop the loader */
-              if (downloadedCocktails.length < 425) {
-                downloadedCocktails.push(newCocktail);
-              } else {
-                setIsLoading(false);
-                setCocktails(downloadedCocktails);
-              }
+      )
+    ).then((responses) => {
+      const allCocktails = responses.reduce(
+        (allPreviousCocktails, response) => {
+          const cocktailsForThisLetter = response.data.drinks.map(
+            ({
+              idDrink: id,
+              strDrink: name,
+              strCategory: category,
+              strAlcoholic: alcoholic,
+              strGlass: glass,
+              strInstructions: instructions,
+              strDrinkThumb: image,
+              ...cocktail
+            }) => ({
+              id,
+              name,
+              category,
+              alcoholic,
+              glass,
+              instructions,
+              image,
+              ingredients: Object.entries(cocktail)
+                .filter(
+                  (key) =>
+                    key.includes("strIngredient") && cocktail[key] != null
+                )
+                .map((key) => cocktail[key]),
+              measures: Object.entries(cocktail)
+                .filter(
+                  (key) => key.includes("strMeasure") && cocktail[key] != null
+                )
+                .map((key) => cocktail[key]),
             })
           );
-        });
-      }
+
+          return [...allPreviousCocktails, ...cocktailsForThisLetter];
+        },
+        []
+      );
+
+      setCocktails(allCocktails);
+      setIsLoading(false);
     });
   }
 
