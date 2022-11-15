4f32cc41b818e5a07dcaa156fbdda778c089ab5f
jessy hazart <jessy.hazart@gmail.com>
2022-11-15 11:17:18 +0100

    Download all cocktails from the api, set new keys for cocktails values and set a loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import "./App.css";
-import allCocktails from "./data/allCocktails";
+import useFetch from "./data/allCocktails";
 
 function App() {
   const [userSearch, setUserSearch] = useState("");
   const [searchValue, setSearchValue] = useState("");
   /* const [actvieSearch, setActiveSearch] = useState(false); */
-  const [cocktailsList, setCocktailsList] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSearch}`;
+  /* const [cocktailsList, setCocktailsList] = useState([]); */
+  const { cocktails, isLoading } = useFetch();
 
   function handleChange(e) {
     setUserSearch(e.target.value);
@@ -27,17 +24,6 @@ function App() {
     }
   }
 
-  const fetchApi = () => {
-    axios.get(url).then((res) => {
-      setCocktailsList(res.data.drinks);
-      setLoading(false);
-    });
-  };
-
-  useEffect(() => {
-    fetchApi();
-  }, [url]);
-
   return (
     <div className="App">
       <Header
@@ -45,11 +31,11 @@ function App() {
         onSubmit={(e) => handleSubmit(e)}
         onChange={(e) => handleChange(e)}
       />
-      {!loading && cocktailsList ? (
-        <Section searchValue={searchValue} cocktailsList={allCocktails} />
+      {!isLoading && cocktails ? (
+        <Section searchValue={searchValue} cocktailsList={cocktails} />
       ) : (
         <p className="search-not-found">
-          {loading ? "Loading cocktails..." : "No matching result"}
+          {isLoading ? "Loading cocktails..." : "No matching result"}
         </p>
       )}
     </div>
--- frontend/src/data/allCocktails.js
+++ frontend/src/data/allCocktails.js
@@ -1,38 +1,68 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 
-const allCocktails = [];
-
-const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
+const downloadedCocktails = [];
 
 const alphabet = "abcdefghijklmnopqrstvwyz".split("");
 
-const fetchApi = (letter) => {
-  axios.get(`${url}${letter}`).then((res) => {
-    res.data.drinks.forEach((cocktail) => {
-      const newCocktail = {
-        id: cocktail.idDrink,
-        name: cocktail.strDrink,
-        category: cocktail.strCategory,
-        alcoholic: cocktail.strAlcoholic,
-        glass: cocktail.strGlass,
-        instructions: cocktail.strInstructions,
-        image: cocktail.strDrinkThumb,
-        ingredients: [],
-        measures: [],
-      };
-      for (const [key, value] of Object.entries(cocktail)) {
-        if (key.includes("strIngredient") && value != null) {
-          newCocktail.ingredients.push(value);
-        }
-        if (key.includes("strMeasure") && value != null) {
-          newCocktail.measures.push(value);
-        }
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [cocktails, setCocktails] = useState([]);
+
+  function fetchApi() {
+    const promises = [];
+    /* fetch the api for each alphabet letter(excepted for u and x) to download all cocktails data */
+    alphabet.forEach((letter) => {
+      const fetch = axios
+        .get(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
+        )
+        .then((res) => res);
+      promises.push(fetch);
+      if (promises.length === 24) {
+        /* if all promises are inserted into an array, set new keys for values of all cocktails */
+        Promise.all([...promises]).then((res) => {
+          res.forEach((data) =>
+            data.data.drinks.forEach((cocktail) => {
+              const newCocktail = {
+                id: cocktail.idDrink,
+                name: cocktail.strDrink,
+                category: cocktail.strCategory,
+                alcoholic: cocktail.strAlcoholic,
+                glass: cocktail.strGlass,
+                instructions: cocktail.strInstructions,
+                image: cocktail.strDrinkThumb,
+                ingredients: [],
+                measures: [],
+              };
+              /* insert ingredients and measures in their arrays */
+              for (const [key, value] of Object.entries(cocktail)) {
+                if (key.includes("strIngredient") && value != null) {
+                  newCocktail.ingredients.push(value);
+                }
+                if (key.includes("strMeasure") && value != null) {
+                  newCocktail.measures.push(value);
+                }
+              }
+              /* insert cocktail object in an array if all cocktails are not downloaded and stop the loader */
+              if (downloadedCocktails.length < 425) {
+                downloadedCocktails.push(newCocktail);
+              } else {
+                setIsLoading(false);
+                setCocktails(downloadedCocktails);
+              }
+            })
+          );
+        });
       }
-      allCocktails.push(newCocktail);
     });
-  });
+  }
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return { cocktails, isLoading };
 };
 
-alphabet.forEach((letter) => fetchApi(letter));
-
-export default allCocktails;
+export default useFetch;
