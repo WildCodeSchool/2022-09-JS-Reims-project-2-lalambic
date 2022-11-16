d2f1cb433113be1f32b159bb0e96dec0a95a0ff5
farid <faridhimeur@outlook.fr>
2022-11-16 16:19:24 +0100

    add css of filterpage

--- frontend/src/components/FilterPage.jsx
+++ frontend/src/components/FilterPage.jsx
@@ -3,18 +3,38 @@ import "./FilterPage.css";
 
 function FilterPage({ setIsShow }) {
   return (
-    <div>
-      <form>
-        <div>
-          <input type="checkbox" id="scales" name="scales" />
-          <label htmlFor="scales">CITRON</label>
+    <div className="display-filter">
+      <form className="formfilter">
+        <div className="inputfilter">
+          <label htmlFor="scales">LEMON</label>
+          <input className="input" type="checkbox" id="scales" name="scales" />
         </div>
-        <div>
-          <input type="checkbox" id="horns" name="horns" />
-          <label htmlFor="horns">Horns</label>
+        <div className="inputfilter">
+          <label htmlFor="scales">STRAWBERRY</label>
+          <input className="input" type="checkbox" id="scales" name="scales" />
+        </div>
+        <div className="inputfilter">
+          <label htmlFor="scales">VODKA</label>
+          <input className="input" type="checkbox" id="scales" name="scales" />
+        </div>
+        <div className="inputfilter">
+          <label htmlFor="scales">WHISKY</label>
+          <input className="input" type="checkbox" id="scales" name="scales" />
+        </div>
+        <div className="inputfilter">
+          <label htmlFor="scales">APPLEJACK</label>
+          <input className="input" type="checkbox" id="scales" name="scales" />
+        </div>
+        <div className="inputfilter">
+          <label htmlFor="scales">KIWI</label>
+          <input className="input" type="checkbox" id="scales" name="scales" />
         </div>
       </form>
-      <button type="button" onClick={() => setIsShow("home")}>
+      <button
+        className="validatefilter"
+        type="button"
+        onClick={() => setIsShow("home")}
+      >
         VALIDER
       </button>
     </div>
